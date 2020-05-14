d068d1f1ed3f8804d58e76dc3f4151d9a10c685b
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2020-05-14 18:08:27 +0530

    Project works

---
+++ backend/src/lambda/http/getTodos.ts
@@ -0,0 +1,31 @@
+import 'source-map-support/register'
+import { getUserId } from '../utils'
+import { createLogger } from '../../utils/logger'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler
+} from 'aws-lambda'
+import { getAllTodos } from '../../businessLogic/ToDo'
+
+const logger = createLogger('getTodos.ts')
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  logger.info('Going to event: ', event)
+
+  const user = getUserId(event)
+  const items = await getAllTodos(user)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      items
+    })
+  }
+}
